98bb37dd60412624b796701a949f4cf61e4a6a1a
emyhrberg <erik.myhrberg@hotmail.com>
2025-02-05 02:51:29 +0100

    Godmode drawing works!

---
+++ src/GodModePlayer.cs
@@ -0,0 +1,41 @@
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Terraria.GameInput;
+
+namespace SquidTestingMod
+{
+    public class GodModePlayer : ModPlayer
+    {
+        public static bool GodMode;
+        private static ModKeybind ToggleGodModeKeybind;
+
+        public override void Load()
+        {
+            ToggleGodModeKeybind = KeybindLoader.RegisterKeybind(Mod, "Toggle God Mode", Keys.H);
+        }
+
+        public override void Unload()
+        {
+            ToggleGodModeKeybind = null;
+        }
+
+        public override void ProcessTriggers(TriggersSet triggersSet)
+        {
+            if (ToggleGodModeKeybind.JustPressed)
+            {
+                GodMode = !GodMode;
+                Main.NewText($"God Mode: {(GodMode ? "Enabled" : "Disabled")}", GodMode ? Color.Green : Color.Red);
+            }
+        }
+
+        // HOOKS, SETTING DAMAGE TO 0.
+        // Todo does it work for drowning/lava? lol not important
+        public override bool CanBeHitByNPC(NPC npc, ref int cooldownSlot) => !GodMode;
+        public override void ModifyHitByNPC(NPC npc, ref Player.HurtModifiers modifiers) { if (GodMode) modifiers.FinalDamage *= 0; }
+        public override void OnHitByProjectile(Projectile proj, Player.HurtInfo hurtInfo) { if (GodMode) hurtInfo.Damage = 0; }
+
+    }
+}
