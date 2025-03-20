febceac5ec66b804e8d24dc78fa341b98fd5d502
emyhrberg <erik.myhrberg@hotmail.com>
2025-03-20 20:09:52 +0100

    Disabled mods functionality works!

--- UI/Elements/ModEnabledText.cs
+++ UI/Elements/ModEnabledText.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text.Json;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using SquidTestingMod.Helpers;
+using Terraria;
 using Terraria.GameContent.UI.Elements;
 using Terraria.ModLoader;
 using Terraria.ModLoader.UI;
@@ -32,43 +34,34 @@ namespace SquidTestingMod.UI.Elements
         public override void LeftClick(UIMouseEvent evt)
         {
             base.LeftClick(evt);
-
-            // remove from the list
-            MainSystem sys = ModContent.GetInstance<MainSystem>();
-            ModsPanel modsPanel = sys.mainState.modsPanel;
-
-            Log.Info("Clicked on internal: " + internalModName);
-
-            for (int i = modsPanel.enabledMods.Count - 1; i >= 0; i--)
+            Log.Info("Clicked on mod: " + internalModName);
+            // Toggle state
+            bool newState = !isEnabled;
+            // Use reflection to invoke the internal method
+            var setModEnabled = typeof(ModLoader).GetMethod("SetModEnabled", BindingFlags.NonPublic | BindingFlags.Static);
+            if (setModEnabled != null)
             {
-                string modNameEnabledMod = modsPanel.enabledMods[i];
-                Log.Info("Checking " + modNameEnabledMod);
-
-                if (modNameEnabledMod == internalModName)
-                {
-                    Log.Info("Removing " + modNameEnabledMod);
-                    modsPanel.enabledMods.RemoveAt(i);
-
-                    // Edit enabled.json and remove the mod from the list and save the file
-                    string file = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Documents\\My Games\\Terraria\\tModLoader\\Mods\\enabled.json";
-
-                    if (File.Exists(file))
-                    {
-                        string json = File.ReadAllText(file);
-
-                        List<string> mods = JsonSerializer.Deserialize<List<string>>(json);
-                        mods.Remove(internalModName);
-
-                        string updatedJson = JsonSerializer.Serialize(mods, new JsonSerializerOptions { WriteIndented = true });
-                        File.WriteAllText(file, updatedJson);
-
-                        Log.Info($"Removed {internalModName} from the list successfully!");
-                    }
-                }
+                setModEnabled.Invoke(null, new object[] { internalModName, newState });
+                Log.Info($"{(newState ? "Enabled" : "Disabled")} {internalModName} via reflection.");
             }
-
-            // toggle text
-            isEnabled = !isEnabled;
+            else
+            {
+                Log.Error("Could not find SetModEnabled method via reflection.");
+            }
+            // Update the mods panel's enabled mods list
+            var sys = ModContent.GetInstance<MainSystem>();
+            var modsPanel = sys.mainState.modsPanel;
+            if (newState)
+            {
+                if (!modsPanel.enabledMods.Contains(internalModName))
+                    modsPanel.enabledMods.Add(internalModName);
+            }
+            else
+            {
+                modsPanel.enabledMods.Remove(internalModName);
+            }
+            // Toggle UI state
+            isEnabled = newState;
             TextColor = isEnabled ? Color.Green : Color.Red;
             SetText(isEnabled ? "Enabled" : "Disabled");
         }
