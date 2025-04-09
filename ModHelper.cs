62604fdd247d0080920731e17216b43844c97e4b
Cotlim <103321004+Cotlim@users.noreply.github.com>
2025-04-09 16:34:52 +0300

    Reload with Publicizier done

--- ModHelper.cs
+++ ModHelper.cs
@@ -1,9 +1,13 @@
 using Basic.Reference.Assemblies;
+using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+using log4net;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Emit;
 using ModHelper.Helpers;
 using ModHelper.PacketHandlers;
+using ModHelper.Publicizier;
 using MonoMod.RuntimeDetour;
 using System;
 using System.Collections.Generic;
@@ -13,6 +17,7 @@ using System.Reflection;
 using System.Text;
 using Terraria;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Core;
 
 namespace ModHelper
 {
@@ -42,44 +47,105 @@ namespace ModHelper
             RoslynCompileHook = new Hook(roslynCompileMethod,
                 (RoslynCompileDelegate orig, string name, List<string> references, string[] files, string[] preprocessorSymbols, bool allowUnsafe, out byte[] code, out byte[] pdb) =>
                 {
-                    Diagnostic[] r = orig(name, references, files, preprocessorSymbols, allowUnsafe, out code, out pdb);
-
-                    var options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary,
-            assemblyIdentityComparer: DesktopAssemblyIdentityComparer.Default,
-            optimizationLevel: preprocessorSymbols.Contains("DEBUG") ? OptimizationLevel.Debug : OptimizationLevel.Release,
-            allowUnsafe: allowUnsafe);
-
-                    var parseOptions = new CSharpParseOptions(LanguageVersion.Preview, preprocessorSymbols: preprocessorSymbols);
-
-                    var emitOptions = new EmitOptions(debugInformationFormat: DebugInformationFormat.PortablePdb);
-
-                    //Where magic happens
-
-                    foreach (var reference in references)
+                    try
                     {
-                        Log.Info($"Reference: {reference}");
+                        Log.Info("RoslynCompileHook called");
+                        //Diagnostic[] r = orig(name, references, files, preprocessorSymbols, allowUnsafe, out code, out pdb);
+
+                        var options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary,
+                assemblyIdentityComparer: DesktopAssemblyIdentityComparer.Default,
+                optimizationLevel: preprocessorSymbols.Contains("DEBUG") ? OptimizationLevel.Debug : OptimizationLevel.Release,
+                allowUnsafe: true);
+
+                        var parseOptions = new CSharpParseOptions(LanguageVersion.Preview, preprocessorSymbols: preprocessorSymbols);
+
+                        var emitOptions = new EmitOptions(debugInformationFormat: DebugInformationFormat.PortablePdb);
+
+                        //Where magic happens
+
+                        var tmlReference = references.FirstOrDefault(s => Path.GetFileNameWithoutExtension(s) == "tModLoader");
+
+                        if (tmlReference != null)
+                        {
+                            references.Remove(tmlReference);
+                        }
+
+                        foreach (var reference in references)
+                        {
+                            Log.Info("Raw Reference: " + reference);
+                        }
+
+                        using ModuleDef module = ModuleDefMD.Load(tmlReference);
+                        Log.Info(PublicizeAssemblies.PublicizeAssembly(module) ? "Module is changed!" : "Someething wrong with module");
+
+                        var writerOptions = new ModuleWriterOptions(module)
+                        {
+                            // Writing the module sometime fails without this flag due to how it was originally compiled.
+                            // https://github.com/krafs/Publicizer/issues/42
+                            MetadataOptions = new MetadataOptions(MetadataFlags.KeepOldMaxStack),
+                            Logger = DummyLogger.NoThrowInstance
+                        };
+
+                        MemoryStream modDefStream = new MemoryStream();
+                        module.Write(modDefStream, writerOptions);
+
+                        // 2. 
+                        var modReference = MetadataReference.CreateFromImage(modDefStream.ToArray());
+
+                        var refs = references.Select(s => MetadataReference.CreateFromFile(s));
+                        refs = refs.Concat(Net80.References.All);
+
+                        refs = refs.Append(modReference);
+
+                        foreach (var reference in refs)
+                        {
+                            Log.Info("True Reference: " + reference.FilePath);
+                        }
+
+
+                        var src = files.Select(f => SyntaxFactory.ParseSyntaxTree(System.IO.File.ReadAllText(f), parseOptions, f, Encoding.UTF8));
+
+                        // IACT 1
+                        const string asmAttr = @"[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksTo(""tModLoader"")]";
+
+                        // IACT 2
+                        const string attrDecl = @"
+                        namespace System.Runtime.CompilerServices {
+                            [System.AttributeUsage(System.AttributeTargets.Assembly, AllowMultiple = true)]
+                            internal sealed class IgnoresAccessChecksToAttribute : System.Attribute {
+                                public IgnoresAccessChecksToAttribute(string assemblyName) {}
+                            }
+                        }
+                        ";
+
+                        // adding IACT to the top of the file
+                        src = src
+                            .Prepend(SyntaxFactory.ParseSyntaxTree(asmAttr, parseOptions)) // має бути першим!
+                            .Append(SyntaxFactory.ParseSyntaxTree(attrDecl, parseOptions));
+
+                        var comp = CSharpCompilation.Create(name, src, refs, options);
+
+                        using var peStream = new MemoryStream();
+                        using var pdbStream = new MemoryStream();
+                        var results = comp.Emit(peStream, pdbStream, options: emitOptions);
+
+                        code = peStream.ToArray();
+                        pdb = pdbStream.ToArray();
+
+                        RoslynCompileHook?.Dispose();
+                        return results.Diagnostics.Where(d => d.Severity >= DiagnosticSeverity.Warning).ToArray();
                     }
-
-                    var refs = references.Select(s => MetadataReference.CreateFromFile(s));
-                    refs = refs.Concat(Net80.References.All);
-
-                    var src = files.Select(f => SyntaxFactory.ParseSyntaxTree(System.IO.File.ReadAllText(f), parseOptions, f, Encoding.UTF8));
-
-                    var comp = CSharpCompilation.Create(name, src, refs, options);
-
-                    using var peStream = new MemoryStream();
-                    using var pdbStream = new MemoryStream();
-                    var results = comp.Emit(peStream, pdbStream, options: emitOptions);
-
-                    code = peStream.ToArray();
-                    pdb = pdbStream.ToArray();
-                    Log.Info("RoslynCompileHook called");
-                    RoslynCompileHook?.Dispose();
-                    Log.Info("RoslynCompileHook disposed");
-                    return results.Diagnostics.Where(d => d.Severity >= DiagnosticSeverity.Warning).ToArray();
+                   catch (Exception)
+                    {
+                        Log.Error("RoslynCompileHook error!");
+                        RoslynCompileHook?.Dispose();
+                        throw;
+                    }
 
                 });
             GC.SuppressFinalize(RoslynCompileHook);
+
+            //LocalMod[] l = [];
         }
 
         public override void HandlePacket(BinaryReader reader, int whoAmI)
---
+++ Publicizier/PublicizeAssemblies.cs
@@ -0,0 +1,139 @@
+﻿using ModHelper.Helpers;
+using Mono.CompilerServices.SymbolWriter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+
+namespace ModHelper.Publicizier
+{
+    internal class PublicizeAssemblies
+    {
+        public static bool PublicizeAssembly(ModuleDef module)
+        {
+            bool publicizedAnyMemberInAssembly = false;
+
+            bool includeVirtual = false; // Include virtual members in the publicization process
+
+            var doNotPublicizePropertyMethods = new HashSet<MethodDef>();
+
+            int publicizedTypesCount = 0;
+            int publicizedPropertiesCount = 0;
+            int publicizedMethodsCount = 0;
+            int publicizedFieldsCount = 0;
+
+            foreach (TypeDef? typeDef in module.GetTypes())
+            {
+                doNotPublicizePropertyMethods.Clear();
+
+                bool publicizedAnyMemberInType = false;
+                string typeName = typeDef.ReflectionFullName;
+
+                bool explicitlyDoNotPublicizeType = false; //assemblyContext.DoNotPublicizeMemberPatterns.Contains(typeName);
+
+                // PROPERTIES
+                foreach (PropertyDef? propertyDef in typeDef.Properties)
+                {
+                    string propertyName = $"{typeName}.{propertyDef.Name}";
+
+                    bool isCompilerGeneratedProperty = IsCompilerGenerated(propertyDef);
+                    if (isCompilerGeneratedProperty)
+                    {
+                        continue;
+                    }
+
+                    if (AssemblyEditor.PublicizeProperty(propertyDef, includeVirtual))
+                    {
+                        publicizedAnyMemberInType = true;
+                        publicizedAnyMemberInAssembly = true;
+                        publicizedPropertiesCount++;
+                    }
+                }
+
+                // METHODS
+                foreach (MethodDef? methodDef in typeDef.Methods)
+                {
+                    string methodName = $"{typeName}.{methodDef.Name}";
+
+                    bool isMethodOfNonPublicizedProperty = doNotPublicizePropertyMethods.Contains(methodDef);
+                    if (isMethodOfNonPublicizedProperty)
+                    {
+                        continue;
+                    }
+
+                    bool isCompilerGeneratedMethod = IsCompilerGenerated(methodDef);
+                    if (isCompilerGeneratedMethod)
+                    {
+                        continue;
+                    }
+
+                    if (AssemblyEditor.PublicizeMethod(methodDef, includeVirtual))
+                    {
+                        publicizedAnyMemberInType = true;
+                        publicizedAnyMemberInAssembly = true;
+                        publicizedMethodsCount++;
+                    }
+                }
+
+                // FIELDS
+                foreach (FieldDef? fieldDef in typeDef.Fields)
+                {
+                    string fieldName = $"{typeName}.{fieldDef.Name}";
+
+                    bool isCompilerGeneratedField = IsCompilerGenerated(fieldDef);
+                    if (isCompilerGeneratedField)
+                    {
+                        continue;
+                    }
+
+                    if (AssemblyEditor.PublicizeField(fieldDef))
+                    {
+                        publicizedAnyMemberInType = true;
+                        publicizedAnyMemberInAssembly = true;
+                        publicizedFieldsCount++;
+                    }
+
+                }
+
+                if (publicizedAnyMemberInType)
+                {
+                    if (AssemblyEditor.PublicizeType(typeDef))
+                    {
+                        publicizedAnyMemberInAssembly = true;
+                        publicizedTypesCount++;
+                    }
+                    continue;
+                }
+
+
+                    bool isCompilerGeneratedType = IsCompilerGenerated(typeDef);
+                    if (isCompilerGeneratedType)
+                    {
+                        continue;
+                    }
+
+                    if (AssemblyEditor.PublicizeType(typeDef))
+                    {
+                        publicizedAnyMemberInAssembly = true;
+                        publicizedTypesCount++;
+                    }
+                
+            }
+
+            Log.Info("Publicized types: " + publicizedTypesCount);
+            Log.Info("Publicized properties: " + publicizedPropertiesCount);
+            Log.Info("Publicized methods: " + publicizedMethodsCount);
+            Log.Info("Publicized fields: " + publicizedFieldsCount);
+
+            return publicizedAnyMemberInAssembly;
+        }
+        private static bool IsCompilerGenerated(IHasCustomAttribute memberDef)
+        {
+            return memberDef.CustomAttributes.Any(x => x.TypeFullName == "System.Runtime.CompilerServices.CompilerGeneratedAttribute");
+        }
+
+    }
+}
