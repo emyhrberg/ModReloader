8bd2a88de1f10a9f2858c6db5990f68613138ac5
Cotlim <103321004+Cotlim@users.noreply.github.com>
2025-04-09 13:02:46 +0300

    Removing Publicizer exept ModInfoState.cs

--- ModHelper.cs
+++ ModHelper.cs
@@ -1,12 +1,18 @@
+using Basic.Reference.Assemblies;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
 using ModHelper.Helpers;
 using ModHelper.PacketHandlers;
 using MonoMod.RuntimeDetour;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Terraria;
 using Terraria.ModLoader;
-using Terraria.ModLoader.Core;
 
 namespace ModHelper
 {
@@ -18,26 +24,60 @@ namespace ModHelper
         public static ModHelper Instance { get; private set; }
 
         public delegate Diagnostic[] RoslynCompileDelegate(
-            string path,
+            string name,
             List<string> references,
-            string[] assemblyReferences,
-            string[] sourceFiles,
-            bool isDebug,
-            out byte[] assemblyBytes,
-            out byte[] pdbBytes);
+            string[] files,
+            string[] preprocessorSymbols,
+            bool allowUnsafe,
+            out byte[] code,
+            out byte[] pdb);
 
         public override void Load()
         {
             Instance = this;
             Hook RoslynCompileHook = null;
-            RoslynCompileHook = new Hook(((RoslynCompileDelegate)ModCompile.RoslynCompile).Method, 
-                (RoslynCompileDelegate orig, string path, List<string> references, string[] assemblyReferences, string[] sourceFiles, bool isDebug, out byte[] assemblyBytes, out byte[] pdbBytes) =>
+            Assembly tModLoaderAssembly = typeof(Main).Assembly;
+            Type modCompileType = tModLoaderAssembly.GetType("Terraria.ModLoader.Core.ModCompile");
+            MethodInfo roslynCompileMethod = modCompileType.GetMethod("RoslynCompile", BindingFlags.NonPublic | BindingFlags.Static);
+            RoslynCompileHook = new Hook(roslynCompileMethod,
+                (RoslynCompileDelegate orig, string name, List<string> references, string[] files, string[] preprocessorSymbols, bool allowUnsafe, out byte[] code, out byte[] pdb) =>
                 {
-                    Diagnostic[] r = orig(path, references, assemblyReferences, sourceFiles, isDebug, out assemblyBytes, out pdbBytes);
+                    Diagnostic[] r = orig(name, references, files, preprocessorSymbols, allowUnsafe, out code, out pdb);
+
+                    var options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary,
+            assemblyIdentityComparer: DesktopAssemblyIdentityComparer.Default,
+            optimizationLevel: preprocessorSymbols.Contains("DEBUG") ? OptimizationLevel.Debug : OptimizationLevel.Release,
+            allowUnsafe: allowUnsafe);
+
+                    var parseOptions = new CSharpParseOptions(LanguageVersion.Preview, preprocessorSymbols: preprocessorSymbols);
+
+                    var emitOptions = new EmitOptions(debugInformationFormat: DebugInformationFormat.PortablePdb);
+
+                    //Where magic happens
+
+                    foreach (var reference in references)
+                    {
+                        Log.Info($"Reference: {reference}");
+                    }
+
+                    var refs = references.Select(s => MetadataReference.CreateFromFile(s));
+                    refs = refs.Concat(Net80.References.All);
+
+                    var src = files.Select(f => SyntaxFactory.ParseSyntaxTree(System.IO.File.ReadAllText(f), parseOptions, f, Encoding.UTF8));
+
+                    var comp = CSharpCompilation.Create(name, src, refs, options);
+
+                    using var peStream = new MemoryStream();
+                    using var pdbStream = new MemoryStream();
+                    var results = comp.Emit(peStream, pdbStream, options: emitOptions);
+
+                    code = peStream.ToArray();
+                    pdb = pdbStream.ToArray();
                     Log.Info("RoslynCompileHook called");
                     RoslynCompileHook?.Dispose();
-                    return r;
-                    
+                    Log.Info("RoslynCompileHook disposed");
+                    return results.Diagnostics.Where(d => d.Severity >= DiagnosticSeverity.Warning).ToArray();
+
                 });
             GC.SuppressFinalize(RoslynCompileHook);
         }
